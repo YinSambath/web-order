7f66d5f83d7b8239de972f7d48c0270cd2cc62d7
YinSambath <72495531+YinSambath@users.noreply.github.com>
2023-01-09 16:53:57 +0700

    Done order page view

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -6,81 +6,100 @@
         </div>
         <div class="bottom-detail">
             <div class="first-detail">
-                <p class="item-name">Chili Sauce Roasted Star Duck</p>
+                <h5 class="item-name">Chili Sauce Roasted Star Duck</h5>
                 <div class="item-price-quantity">
                     <p class="price">$14.50</p>
+                    <div class="quatity">
+                        <el-button @click.prevent="decrement" :disabled="quantity == 1" class="btn-minus" circle>
+                            <img :src="minus" />
+                        </el-button>
+                        <p>{{ quantity }}</p>
+                        <el-button @click.prevent="increment" class="btn-plus" circle>
+                            <img :src="plus" />
+                        </el-button>
+                    </div>
                 </div>
             </div>
+            <el-divider />
+            <div class="description">
+                <h5>Chili Sauce Roasted Star Duck</h5>
+                <p>Fresh Duck with 3 sizes Mixed with chili and it's really popular for dinner with family and friends.</p>
+            </div>
+            <el-divider />
+            <div class="size">
+                <h5>Size</h5>
+                <el-radio-group v-model="size" @change="addSize(size)">
+                    <el-radio label="s" size="default">S</el-radio>
+                    <el-radio label="m" size="default">M</el-radio>
+                    <el-radio label="l" size="default">L</el-radio>
+                </el-radio-group>
+            </div>
+            <el-divider />
+            <div class="additional">
+                <span>
+                    <h5>Frequenly bought together</h5>
+                    <el-tag type="info" size="small" round>Optional</el-tag>
+                </span>
+                <el-radio-group v-model="additional" @change="addAdditional(additional)">
+                    <el-radio label="1" size="default">
+                        <p>Duck chili 125%</p>
+                        <p>12.5$</p>
+                    </el-radio>
+                    <el-radio label="2" size="default">
+                        <p>Duck chili 100%</p>
+                        <p>10.5$</p>
+                    </el-radio>
+                </el-radio-group>
+                <!-- <el-checkbox label="Online activities" name="type" /> -->
+            </div>
+            <el-button-group class="btn-submit">
+                <el-button class="add-to-cart">Add to cart</el-button>
+                <el-button class="buy-now">Buy now</el-button>
+            </el-button-group>
         </div>
     </div>
 </template>
 <script>
+import {ref} from 'vue';
 import item1 from '@/assets/icons/item-2.png'
 import arrowBack from '@/assets/icons/arrow-back.png'
+import minus from '@/assets/icons/minus.png'
+import plus from '@/assets/icons/plus.png'
 export default {
     name: "OrderView",
     data() {
         return {
-            item1, arrowBack
+            item1, arrowBack, minus, plus
         }
     },
     setup() {
+        const quantity = ref(1);
+        const size = ref({});
+        const additional = ref({});
+
+        return {
+            quantity,
+            size, 
+            additional,
+        }
     },
     methods: {
         goBack() {
             this.$router.back();
         },
+        increment() {
+            this.quantity++;
+        },
+        decrement() {
+            this.quantity-- ;
+        },
+        addSize(value) {
+            console.log(value);
+        },
+        addAdditional(value) {
+            console.log(value);
+        },
     }
 }
 </script>
-<style scoped lang="scss">
-    .order-detail {
-        min-height: 100vh;
-        background-color: #ffffff;
-
-        .top-detail {
-            height: 60vh;
-            background-color: #FFE8EE;
-            border-radius: 0 0 40px 40px;
-            display: flex;
-            position: relative;
-
-            .arrow-left {
-                position: absolute;
-                top: 25px;
-                left: 25px;
-                width: 30px;
-                height: 30px;
-            }
-            .image-detail {
-                margin: auto;
-                max-width: 75vw;
-                max-height: 45vh;
-                border-radius: 50%;
-            }
-        }
-        .bottom-detail {
-            padding: 3vw;
-            height: auto;
-
-            .first-detail {
-                .item-name {
-                    margin: 0;
-                    font-size: 1rem;
-                    font-weight: bold;
-                    text-align: start;
-                }
-                .item-price-quantity {
-                    display: flex;
-
-                    .price {
-                        margin: 10px 0;
-                        font-size: 1.1rem;
-                        color: #FF6464;
-                        font-weight: bold;
-                    }
-                }
-            }
-        }
-    }
-</style>
+<style scoped lang="scss" src="@/styles/order.scss"></style>
